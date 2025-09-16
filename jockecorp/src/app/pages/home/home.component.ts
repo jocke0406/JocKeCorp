5b17f4f9c0c5a87d15e2a5eb4e5f6209ee4aa53e
jocke0406 <jocke0406@gmail.com>
2025-09-16 17:37:47 +0200

    Enter inside

--- jockecorp/src/app/pages/home/home.component.ts
+++ jockecorp/src/app/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
